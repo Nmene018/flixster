c8e550fabde4456788c57174c027e1f4621061bb
Natalie018 <57768980+Nmene018@users.noreply.github.com>
2020-09-23 02:10:25 -0400

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Natalie Meneses on 9/23/20.
+//  Copyright © 2020 NatalieM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
