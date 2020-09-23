104cbf7b6c886aaf8b75c48d9566ef6d9ac90dd5
Natalie018 <57768980+Nmene018@users.noreply.github.com>
2020-09-23 06:07:08 -0400

    flixster

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Natalie Meneses on 9/23/20.
+//  Copyright © 2020 NatalieM. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
